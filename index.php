b0d59e275e3ac8145f7a80e78ea423bf923b9978
LittleThigas <fatalzerax@gmail.com>
2024-10-29 10:07:37 -0300

    pagina cadastro e css

--- index.php
+++ index.php
@@ -4,10 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="elementos.css">
+
 </head>
 <body>
     <h1>Walter de Jesus, Thiago Santana </h1>
     <img src="imagens/pizza.png" alt="Pizza" />
+
+    <a href="cadastro.php">
+    <button>Cadastrar</button>
+    </a>
 
 </body>
 </html>
